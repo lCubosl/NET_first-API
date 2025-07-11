5ec7ae4214bf129ad85ab2e8505a53314d777243
LuisM <muislarques@gmail.com>
2025-07-12 00:22:36 +0100

    Add project files.

---
+++ NET_first-API/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace NET_first_API.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public int YearPublished { get; set; }
+    }
+}
